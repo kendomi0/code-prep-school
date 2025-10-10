2b9d1a31447905011af7251015557b5600c35b99
Kendra <kendra.dominique0@gmail.com>
2025-10-10 12:52:02 -0400

    feat: add header .js file
    Add header .js file to repeat header on all pages.

---
+++ header.js
@@ -0,0 +1,15 @@
+let appHeader = `
+    <a href="index.html" class="title">CodePrepSchool</a>
+    <nav class="nav-links">
+        <a href="constant-time-complexity.html" class="nav-item">
+            Constant Time Complexity
+        </a>
+        <a href="linear-time-complexity.html" class="nav-item">
+            Linear Time Complexity
+        </a>
+        <a href="insertion-sort.html" class="nav-item">
+            Insertion Sort
+        </a>
+    </nav>
+`;
+document.getElementById("app-header").innerHTML = appHeader;
