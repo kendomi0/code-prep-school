b4bf00b128f9851b40e500f489408acad5f5318b
Kendra <kendra.dominique0@gmail.com>
2026-04-13 15:00:20 -0400

    feat: add mobile version of header

--- header.js
+++ header.js
@@ -47,3 +47,74 @@ let appHeader = `
     </nav>
 `;
 document.getElementById("app-header").innerHTML = appHeader;
+
+
+
+
+/* Mobile */
+
+let mobileHeader = `
+    <a href="index.html" class="mobile-title">CodePrepSchool</a>
+    
+    <nav class="mobile-nav">
+
+    <div id="mobile-menu-icon">
+        <i class="fa-solid fa-bars fa-xl"></i>
+
+         <div id="full-mobile-dropdown">
+
+            <div class="mobile-dropdown">
+                <div class="mobile-dropdown-title mobile-odd-nav">
+                    Time Complexity
+                    <i class="fa-solid fa-caret-down"></i>
+                </div>
+
+                <div class="mobile-nav-links">
+                    <a href="time-complexity-basics.html" class="mobile-nav-item">
+                        Time Complexity Basics
+                    </a>
+                    <a href="built-in-methods.html" class="mobile-nav-item">
+                        Built-In Methods
+                    </a>
+                    <a href="constant-time-complexity.html" class="mobile-nav-item">
+                        Constant Time Complexity
+                    </a>
+                    <a href="linear-time-complexity.html" class="mobile-nav-item">
+                        Linear Time Complexity
+                    </a>
+                    <a href="quadratic-time-complexity.html" class="mobile-nav-item">
+                        Quadratic Time Complexity
+                    </a>
+                    <a href="time-complexity-quiz.html" class="mobile-nav-item">
+                        Time Complexity Quiz
+                    </a>
+                </div>
+
+            </div>
+
+            <div class="mobile-dropdown" id="dropdown-algorithms">
+
+                <div class="mobile-dropdown-title mobile-even-nav">
+                    Algorithms
+                    <i class="fa-solid fa-caret-down"></i>
+                </div>
+
+                <div class="mobile-nav-links">
+                    <a href="intro-to-algorithms.html" class="mobile-nav-item">
+                        Intro to Algorithms
+                    </a>
+                    <a href="insertion-sort.html" class="mobile-nav-item">
+                        Insertion Sort
+                    </a>
+                    <a href="bubble-sort.html" class="mobile-nav-item">
+                        Bubble Sort
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </nav>
+`;
+
+document.getElementById("mobile-header").innerHTML = mobileHeader;
