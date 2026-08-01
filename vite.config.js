b066bdeb96ce879c1940ef04ce80d26b34841692
Kendra <kendra.dominique0@gmail.com>
2026-08-01 19:23:37 -0400

    style: run prettier

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/code-prep-school/',
+  base: "/code-prep-school/",
   build: {
     rollupOptions: {
       input: {
-        main: './index.html',
-        timeComplexityBasics: './time-complexity-basics.html',
-        builtInMethods: './built-in-methods.html',
-        constantTimeComplexity: './constant-time-complexity.html',
-        linearTimeComplexity: './linear-time-complexity.html',
-        quadraticTimeComplexity: './linear-time-complexity.html',
-        timeComplexityQuiz: './time-complexity-quiz.html',
-        introToAlgorithms: './intro-to-algorithms.html',
-        insertionSort: './insertion-sort.html',
-        bubbleSort: './bubble-sort.html',
-      }
-    }
-  }
-})
+        main: "./index.html",
+        timeComplexityBasics: "./time-complexity-basics.html",
+        builtInMethods: "./built-in-methods.html",
+        constantTimeComplexity: "./constant-time-complexity.html",
+        linearTimeComplexity: "./linear-time-complexity.html",
+        quadraticTimeComplexity: "./linear-time-complexity.html",
+        timeComplexityQuiz: "./time-complexity-quiz.html",
+        introToAlgorithms: "./intro-to-algorithms.html",
+        insertionSort: "./insertion-sort.html",
+        bubbleSort: "./bubble-sort.html",
+      },
+    },
+  },
+});
