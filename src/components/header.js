72b0c304542843dd104cddeca5b218c7baa3a1e8
Kendra D. <kendra.dominique0@gmail.com>
2026-08-11 12:31:31 -0400

    fix: replace repo name in nav links with Vite's BASE_URL (#49)
    The homepage link was returning a 404 error once the site was deployed to Github Pages due to the link leading to /index-html instead of code-prep-school/index-html. Additionally, clicking the header's navigation links on the site's mobile version was resulting in 404 errors, due to the links having not been changed after the files were organized into folders.
    
    Replace the hardcoded repository name in all of the header's navigation links with Vite's import.meta.env.BASE_URL environment variable to correct the homepage error and prevent future bugs. Correct the mobile version's header's navigation links to match the current file paths.

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,8 @@
+const baseURL = `${import.meta.env.BASE_URL}`;
+const homeLink = `${import.meta.env.BASE_URL}index.html`;
+
 let appHeader = `
-        <a href="/index.html" class="title">CodePrepSchool</a>
+        <a href="${homeLink}" class="title">CodePrepSchool</a>
         <nav class="nav-dropdown">
             <div class="dropdown" id="dropdown-time-complexity">
                 <div class="dropdown-title">
@@ -7,22 +10,22 @@ let appHeader = `
                     <i class="fa-solid fa-caret-down"></i>
                 </div>
                 <div class="nav-links">
-                    <a href="/code-prep-school/src/pages/time-complexity-basics/time-complexity-basics.html" class="nav-item">
+                    <a href="${baseURL}src/pages/time-complexity-basics/time-complexity-basics.html" class="nav-item">
                         Time Complexity Basics
                     </a>
-                    <a href="/code-prep-school/src/pages/built-in-methods.html" class="nav-item">
+                    <a href="${baseURL}src/pages/built-in-methods.html" class="nav-item">
                         Built-In Methods
                     </a>
-                    <a href="/code-prep-school/src/pages/constant-time-complexity.html" class="nav-item">
+                    <a href="${baseURL}src/pages/constant-time-complexity.html" class="nav-item">
                         Constant Time Complexity
                     </a>
-                    <a href="/code-prep-school/src/pages/linear-time-complexity.html" class="nav-item">
+                    <a href="${baseURL}src/pages/linear-time-complexity.html" class="nav-item">
                         Linear Time Complexity
                     </a>
-                    <a href="/code-prep-school/src/pages/quadratic-time-complexity.html" class="nav-item">
+                    <a href="${baseURL}src/pages/quadratic-time-complexity.html" class="nav-item">
                         Quadratic Time Complexity
                     </a>
-                    <a href="/code-prep-school/src/pages/time-complexity-quiz/time-complexity-quiz.html" class="nav-item">
+                    <a href="${baseURL}src/pages/time-complexity-quiz/time-complexity-quiz.html" class="nav-item">
                         Time Complexity Quiz
                     </a>
                 </div>
@@ -33,13 +36,13 @@ let appHeader = `
                     <i class="fa-solid fa-caret-down"></i>
                 </div>
                 <div class="nav-links">
-                    <a href="/code-prep-school/src/pages/intro-to-algorithms/intro-to-algorithms.html" class="nav-item">
+                    <a href="${baseURL}src/pages/intro-to-algorithms/intro-to-algorithms.html" class="nav-item">
                         Intro to Algorithms
                     </a>
-                    <a href="/code-prep-school/src/pages/insertion-sort/insertion-sort.html" class="nav-item">
+                    <a href="${baseURL}src/pages/insertion-sort/insertion-sort.html" class="nav-item">
                         Insertion Sort
                     </a>
-                    <a href="/code-prep-school/src/pages/bubble-sort/bubble-sort.html" class="nav-item">
+                    <a href="${baseURL}src/pages/bubble-sort/bubble-sort.html" class="nav-item">
                         Bubble Sort
                     </a>
                 </div>
@@ -54,7 +57,7 @@ if (document.getElementById("app-header")) {
 /* Mobile */
 
 let mobileHeader = `
-    <a href="index.html" class="mobile-title">CodePrepSchool</a>
+    <a href="${homeLink}" class="mobile-title">CodePrepSchool</a>
     
     <nav class="mobile-nav">
 
@@ -70,22 +73,22 @@ let mobileHeader = `
                 </div>
 
                 <div id="time-complexity-links" class="mobile-nav-links">
-                    <a href="time-complexity-basics.html" class="mobile-nav-item">
+                    <a href="${baseURL}src/pages/time-complexity-basics/time-complexity-basics.html" class="mobile-nav-item">
                         Time Complexity Basics
                     </a>
-                    <a href="built-in-methods.html" class="mobile-nav-item">
+                    <a href="${baseURL}src/pages/built-in-methods.html" class="mobile-nav-item">
                         Built-In Methods
                     </a>
-                    <a href="constant-time-complexity.html" class="mobile-nav-item">
+                    <a href="${baseURL}src/pages/constant-time-complexity.html" class="mobile-nav-item">
                         Constant Time Complexity
                     </a>
-                    <a href="linear-time-complexity.html" class="mobile-nav-item">
+                    <a href="${baseURL}src/pages/linear-time-complexity.html" class="mobile-nav-item">
                         Linear Time Complexity
                     </a>
-                    <a href="quadratic-time-complexity.html" class="mobile-nav-item">
+                    <a href="${baseURL}src/pages/quadratic-time-complexity.html" class="mobile-nav-item">
                         Quadratic Time Complexity
                     </a>
-                    <a href="time-complexity-quiz.html" class="mobile-nav-item">
+                    <a href="${baseURL}src/pages/time-complexity-quiz/time-complexity-quiz.html" class="mobile-nav-item">
                         Time Complexity Quiz
                     </a>
                 </div>
@@ -100,13 +103,13 @@ let mobileHeader = `
                 </div>
 
                 <div id="algorithm-links" class="mobile-nav-links">
-                    <a href="intro-to-algorithms.html" class="mobile-nav-item">
+                    <a href="${baseURL}src/pages/intro-to-algorithms/intro-to-algorithms.html" class="mobile-nav-item">
                         Intro to Algorithms
                     </a>
-                    <a href="insertion-sort.html" class="mobile-nav-item">
+                    <a href="${baseURL}src/pages/insertion-sort/insertion-sort.html" class="mobile-nav-item">
                         Insertion Sort
                     </a>
-                    <a href="bubble-sort.html" class="mobile-nav-item">
+                    <a href="${baseURL}src/pages/bubble-sort/bubble-sort.html" class="mobile-nav-item">
                         Bubble Sort
                     </a>
                 </div>
