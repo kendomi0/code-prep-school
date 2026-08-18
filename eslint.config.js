f56cb4f36bc274ff368b27d8d526a1f8ac231465
Kendra <kendra.dominique0@gmail.com>
2026-08-18 15:23:38 -0400

    chore: configure tooling to recognize Vitest globals
    - Install eslint-plugin-vitest-globals and update
    eslint.config.js. Remove the previous rule
    to ignore the __tests__ folder.
    
    - Add Vitest globals to jsconfig.json.

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,20 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import vitestGlobals from 'eslint-plugin-vitest-globals'
 
 export default defineConfig([
-  { 
-    ignores: ["__tests__"]
-  },
   {
     rules: {
       "no-unused-vars": ["error", { argsIgnorePattern: "^_" }]
     }
   },
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.test.js', '**/*.spec.js'],
+    ...vitestGlobals.configs['flat/recommended']
+  }
 ]);
