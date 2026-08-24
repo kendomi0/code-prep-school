eda708ee87bce9f71bff5f388667aefdbb07a121
Kendra <kendra.dominique0@gmail.com>
2026-08-24 12:44:15 -0400

    fix: remove unneeded variable assignment
    Remove the variable assignment for the BubbleSortAnimation() instance,
    as it was causing an "unused variable" lint error.

--- src/pages/bubble-sort/bubble-sort-main.js
+++ src/pages/bubble-sort/bubble-sort-main.js
@@ -4,4 +4,4 @@ import "./bubble-sort-logic";
 import { BubbleSortAnimation } from "./bubble-sort-animation";
 
 const container = document.querySelector(".animation-container");
-let animatedBubbleSort = new BubbleSortAnimation(container);
+new BubbleSortAnimation(container);
